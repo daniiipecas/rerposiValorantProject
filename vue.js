6b54a9f3f2e9e6656fe0ddd51d02880e6f221d96
daniiipecas <danielcamargo444@gmail.com>
2023-11-04 10:20:08 -0500

    cambios nuevos

--- vue.js
+++ vue.js
@@ -1,23 +1,27 @@
 const urlApi = 'https://valorant-api.com/v1/agents';
 
-    new Vue({
-      el: '#app',
-      data: {
-        personaje: {}
-      },
-      methods: {
-        traerInfo() {
-          fetch(urlApi)
-            .then(response => response.json())
-            .then(data => {
-              if (data.data.length > 0) {
-                this.personaje = data.data[0];
-              }
-            });
-        }
-      },
-      created() {
-        this.traerInfo(); 
-      }
-    });
+const app = Vue.createApp({
+  data() {
+    return {
+      personaje: {}
+    };
+  },
+  methods: {
+    traerInfo() {
+      fetch(urlApi)
+        .then(response => response.json())
+        .then(data => {
+          if (data.data.length > 0) {
+            this.personaje = data.data[5];
+          }
+        });
+    }
+  },
+  created() {
+    this.traerInfo();
+  }
+});
+
+app.mount('#app');
+
     
